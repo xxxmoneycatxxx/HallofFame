e44b582182c9b4f2e3558631888789527d67a027
xXx_MoneyCat <guangzhougary@foxmail.com>
2025-08-17 13:50:40 +0800

    更新结构

--- setting.php
+++ setting.php
@@ -102,7 +102,7 @@ const CLASS_JS_ITEMLIST = CLASS_DIR . "class.JS_itemlist.php";
 const CLASS_SMITHY = CLASS_DIR . "class.smithy.php";
 const CLASS_AUCTION = CLASS_DIR . "class.auction.php";
 const GLOBAL_PHP = CLASS_DIR . "global.php";
-const COLOR_FILE = CLASS_DIR . "Color.dat";
+const COLOR_FILE = CLASS_DIR . "../db/Color.dat";
 
 // DATA FILE
 const DATA_DIR = "./data/";
